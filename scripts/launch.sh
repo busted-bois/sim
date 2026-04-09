c1b1b65b924351e7dd8f78b43ff7c2e27cddd14c
therealsamyak <therealsamyak@gmail.com>
2026-04-08 21:26:22 -0700

    no pipefail

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo
-(set -o pipefail 2>/dev/null) && set -o pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_ROOT="$(dirname "$SCRIPT_DIR")"
